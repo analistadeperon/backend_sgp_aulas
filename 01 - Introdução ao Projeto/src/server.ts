d771b249cd481cff9e380bd79468b5852205130c
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2021-06-16 14:17:26 -0300

    back_01

---
+++ 01 - Introdução ao Projeto/src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Server started on port 3333!');
+});
